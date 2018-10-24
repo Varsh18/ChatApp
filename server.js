64306ca1c10a2e568ebdf28444153ef3f05ebd8c
varsh18 <varshiniravi1824@gmail.com>
2018-10-24 10:47:29 +0530

    Run server at port 3000

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ var server=require('http').createServer(app);
 var io=require('socket.io').listen(server);
 users=[];
 connections=[];
+
+// run the server at the port 3000
+server.listen (process.env.PORT||3000);
